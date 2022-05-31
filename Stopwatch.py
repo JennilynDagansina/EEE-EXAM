de14b21656121d970607d61793c80542ada60726
VincentGreggPamor <105401819+VincentGreggPamor@users.noreply.github.com>
2022-05-31 22:34:48 +0800

    Update Stopwatch.py

--- Stopwatch.py
+++ Stopwatch.py
@@ -1,7 +1,6 @@
-from tkinter import *
-from tkinter import messagebox
-import time
 
+from tkinter import *
+import time
 
 class StopWatch(Frame):  
     """ Implements a stop watch frame widget. """                                                                
@@ -19,34 +18,34 @@ class StopWatch(Frame):
         self.laps = []
         self.lapmod2 = 0
         self.today = time.strftime("%d %b %Y %H-%M-%S", time.localtime())
-        
+
     def makeWidgets(self):                         
-        """ Labeling a Stopwatch """
-        title = Label(self, text='Stopwatch', font=('Arial', 20))
-        title.pack(fill=X, expand=YES, pady=1, padx=2)
-        
-        watch = Label(self, textvariable=self.timestr, font=('Arial', 32), bg=('black'), fg=('white'))
+        """ Stopwatch Label """
+        title = Label(self, text='Stopwatch', font=('Arial', 10))
+        title.pack(fill=X, expand=NO, pady=1, padx=2)
+
+        watch = Label(self, textvariable=self.timestr, font=('Arial', 16), bg=('black'), fg=('white'))
         self._setTime(self._elapsedtime)
-        watch.pack(fill=X, expand=YES, pady=3, padx=2)
-        
-        name = Label(self, text= '            hours          minutes        seconds       centisec.       ', font=('Arial', 8), fg='white', bg='black')
-        name.pack(fill='x', expand='yes')
+        watch.pack(fill=X, expand=NO, pady=3, padx=2)
+
+        name = Label(self, text= '           hours            minutes          seconds         centisec.       ', font=('Arial', 4), fg='white', bg='black')
+        name.pack(fill='x', expand='no')
 
         lap = Label(self, text='Laps')
-        lap.pack(fill=X, expand=YES, pady=4, padx=2)
+        lap.pack(fill=X, expand=NO, pady=4, padx=2)
 
         scrollbar = Scrollbar(self, orient=VERTICAL)
-        self.listbox = Listbox(self,selectmode=EXTENDED, height = 8, yscrollcommand=scrollbar.set)
+        self.listbox = Listbox(self,selectmode=EXTENDED, height = 5, yscrollcommand=scrollbar.set)
         self.listbox.pack(side=LEFT, fill=BOTH, expand=1, pady=5, padx=2)
         scrollbar.config(command=self.listbox.yview)
         scrollbar.pack(side=RIGHT, fill=Y)
-   
+
     def _update(self): 
         """ Update the label with elapsed time. """
         self._elapsedtime = time.time() - self._start
         self._setTime(self._elapsedtime)
         self._timer = self.after(50, self._update)
-    
+
     def _setTime(self, elap):
         """ Set the time string to Hours:Minutes:Seconds:Hundreths """
         hours = int(elap/3600.0)
@@ -62,14 +61,14 @@ class StopWatch(Frame):
         seconds = int(elap - minutes*60.0)
         hseconds = int((elap - minutes*60.0 - seconds)*100)            
         return '%02d:%02d:%02d:%02d' % (hours, minutes, seconds, hseconds)
-        
+
     def Start(self):                         
         """ Start the stopwatch, ignore if running. """
         if not self._running:            
             self._start = time.time() - self._elapsedtime
             self._update()
             self._running = 1        
-    
+
     def Stop(self):                                    
         """ Stop the stopwatch, ignore if stopped. """
         if self._running:
@@ -77,7 +76,7 @@ class StopWatch(Frame):
             self._elapsedtime = time.time() - self._start    
             self._setTime(self._elapsedtime)
             self._running = 0
-    
+
     def Reset(self):                                  
         """ Reset the stopwatch. """
         self._start = time.time()         
@@ -86,13 +85,6 @@ class StopWatch(Frame):
         self.lapmod2 = self._elapsedtime
         self._setTime(self._elapsedtime)
         self.listbox.delete(0, END)
-        
-    def ExitApp(self):
-        MsgBox = messagebox.askquestion ('Exit App','Really Quit?',icon = 'error')
-        if MsgBox == 'yes':
-           self.window.destroy()
-        else:
-            messagebox.showinfo('Welcome Back','Welcome back to the App')
 
     def Lap(self):
         '''Makes a lap'''
@@ -102,30 +94,26 @@ class StopWatch(Frame):
             self.listbox.insert(END, self.laps[-1])
             self.listbox.yview_moveto(1)
             self.lapmod2 = self._elapsedtime
-                       
+
     def storing(self):
         '''Get the name of the timer and create a file to store the laps'''
         file = str(self.e.get()) + ' - '
         with open(file + self.today + '.txt', 'wb') as lapfile:
             for lap in self.laps:
-                lapfile.write((bytes(str(lap) + '\n', 'utf-8', font=('Arial', 20))))
-            
+                lapfile.write((bytes(str(lap) + '\n', 'utf-8')))
+
 def main():
     root = Tk()
     root.wm_attributes("-topmost", 1)      #always on top - might do a button for it
-    root.geometry('485x450')
     sw = StopWatch(root)
     sw.pack(side=TOP)
 
+    Button(root, text='Start', height=3, width=4, fg=('green'), bg=('black'), command=sw.Start).pack(side=LEFT)
+    Button(root, text='Stop', height=3, width=4, fg=('red'), bg=('black'), command=sw.Stop).pack(side=LEFT)
+    Button(root, text='Lap', height=3, width=4, fg=('yellow'), bg=('black'), command=sw.Lap).pack(side=LEFT)
+    Button(root, text='Reset', height=3, width=4, fg=('cyan'), bg=('black'), command=sw.Reset).pack(side=LEFT)
 
-    Button(root, text='Start', height=5, width=12, fg=('green'), command=sw.Start).pack(fill=X, expand=YES, pady=3, padx=2, side=LEFT, anchor=CENTER)
-    Button(root, text='Stop', height=5, width=12, fg=('red'), command=sw.Stop).pack(fill=X, expand=YES, pady=3, padx=2, side=LEFT, anchor=CENTER)
-    Button(root, text='Lap', height=5, width=12, fg=('black'), command=sw.Lap).pack(fill=X, expand=YES, pady=3, padx=2, side=LEFT, anchor=CENTER)
-    Button(root, text='Reset', height=5, width=12, fg=('black'), command=sw.Reset).pack(fill=X, expand=YES, pady=3, padx=2, side=LEFT, anchor=CENTER)
-    Button(root, text='Exit App',height=3, width=12, command=sw.ExitApp).pack(fill=X, expand=NO, pady=3, padx=2,anchor=CENTER, side=LEFT)
-    
     root.mainloop()
 
 if __name__ == '__main__':
     main()
-    
