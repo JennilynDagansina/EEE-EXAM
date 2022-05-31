448fdf5cb38b3e29f1a5273d2bb02eaa7702b9d9
Rheelmi <105403897+Rheelmi@users.noreply.github.com>
2022-05-31 22:43:43 +0800

    Update Stopwatch.py

--- Stopwatch.py
+++ Stopwatch.py
@@ -1,12 +1,13 @@
 
+
+
 from tkinter import *
 import time
 
 class StopWatch(Frame):  
-    """ Implements a stop watch frame widget. """                                                                
+    # Executes a stop watch frame widget.                                                                 
     def __init__(self, parent=None, **kw):        
         Frame.__init__(self, parent, kw)
-        self.window = parent
         self._start = 0.0        
         self._elapsedtime = 0.0
         self._running = 0
@@ -18,16 +19,16 @@ class StopWatch(Frame):
         self.laps = []
         self.lapmod2 = 0
         self.today = time.strftime("%d %b %Y %H-%M-%S", time.localtime())
-
+        
     def makeWidgets(self):                         
-        """ Stopwatch Label """
+        # Stopwatch Label 
         title = Label(self, text='Stopwatch', font=('Arial', 10))
         title.pack(fill=X, expand=NO, pady=1, padx=2)
-
+        
         watch = Label(self, textvariable=self.timestr, font=('Arial', 16), bg=('black'), fg=('white'))
         self._setTime(self._elapsedtime)
         watch.pack(fill=X, expand=NO, pady=3, padx=2)
-
+        
         name = Label(self, text= '           hours            minutes          seconds         centisec.       ', font=('Arial', 4), fg='white', bg='black')
         name.pack(fill='x', expand='no')
 
@@ -39,15 +40,15 @@ class StopWatch(Frame):
         self.listbox.pack(side=LEFT, fill=BOTH, expand=1, pady=5, padx=2)
         scrollbar.config(command=self.listbox.yview)
         scrollbar.pack(side=RIGHT, fill=Y)
-
+   
     def _update(self): 
-        """ Update the label with elapsed time. """
+         # update stopwatch function in order to make changes on stopwatch every seconds elapsed time.
         self._elapsedtime = time.time() - self._start
         self._setTime(self._elapsedtime)
         self._timer = self.after(50, self._update)
-
+    
     def _setTime(self, elap):
-        """ Set the time string to Hours:Minutes:Seconds:Hundreths """
+        # Set the time string to Hours:Minutes:Seconds:Hundreths
         hours = int(elap/3600.0)
         minutes = int(elap/60.0)
         seconds = int(elap - minutes*60.0)
@@ -55,30 +56,30 @@ class StopWatch(Frame):
         self.timestr.set('%02d:%02d:%02d:%02d' % (hours, minutes, seconds, hseconds))
 
     def _setLapTime(self, elap):
-        """ Set the time string to Hours:Minutes:Seconds:Hundreths """
+        # Set the time string to Hours:Minutes:Seconds:Hundreths 
         hours = int(elap/3600.0)
         minutes = int(elap/60.0)
         seconds = int(elap - minutes*60.0)
         hseconds = int((elap - minutes*60.0 - seconds)*100)            
         return '%02d:%02d:%02d:%02d' % (hours, minutes, seconds, hseconds)
-
+        
     def Start(self):                         
-        """ Start the stopwatch, ignore if running. """
+        # Start the stopwatch, ignore if running.
         if not self._running:            
             self._start = time.time() - self._elapsedtime
             self._update()
             self._running = 1        
-
+    
     def Stop(self):                                    
-        """ Stop the stopwatch, ignore if stopped. """
+        # Stop the stopwatch, ignore if stopped. 
         if self._running:
             self.after_cancel(self._timer)            
             self._elapsedtime = time.time() - self._start    
             self._setTime(self._elapsedtime)
             self._running = 0
-
+    
     def Reset(self):                                  
-        """ Reset the stopwatch. """
+         # Reset the stopwatch. Reset function is use to set the watch to zero  """
         self._start = time.time()         
         self._elapsedtime = 0.0
         self.laps = []   
@@ -87,21 +88,21 @@ class StopWatch(Frame):
         self.listbox.delete(0, END)
 
     def Lap(self):
-        '''Makes a lap'''
+        # Makes a lap, only if the watch starts running.
         tempo = self._elapsedtime - self.lapmod2
         if self._running:
             self.laps.append(self._setLapTime(tempo))
             self.listbox.insert(END, self.laps[-1])
             self.listbox.yview_moveto(1)
             self.lapmod2 = self._elapsedtime
-
+                       
     def storing(self):
-        '''Get the name of the timer and create a file to store the laps'''
+        # Get the name of the timer and create a file to store the laps
         file = str(self.e.get()) + ' - '
         with open(file + self.today + '.txt', 'wb') as lapfile:
             for lap in self.laps:
                 lapfile.write((bytes(str(lap) + '\n', 'utf-8')))
-
+            
 def main():
     root = Tk()
     root.wm_attributes("-topmost", 1)      #always on top - might do a button for it
@@ -112,8 +113,8 @@ def main():
     Button(root, text='Stop', height=3, width=4, fg=('red'), bg=('black'), command=sw.Stop).pack(side=LEFT)
     Button(root, text='Lap', height=3, width=4, fg=('yellow'), bg=('black'), command=sw.Lap).pack(side=LEFT)
     Button(root, text='Reset', height=3, width=4, fg=('cyan'), bg=('black'), command=sw.Reset).pack(side=LEFT)
-
+    
     root.mainloop()
 
 if __name__ == '__main__':
-    main()
+    
